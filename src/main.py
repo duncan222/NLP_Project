85ae936bac8686223674c295c09134ca5c15456f
chtreutel <claretreutel@proton.me>
2024-03-15 23:41:24 -0400

    added data and split into train/test

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import csv
 
 import tensorflow as tf
 import tensorflow_hub as hub
@@ -12,3 +13,29 @@ import matplotlib.pyplot as plt
 
 tf.get_logger().setLevel('ERROR')
 
+def split_data(combined, keyword, random):
+    with open(combined, 'r', newline='') as combinedcsv, open(keyword, 'w+', newline='') as train, open(random, 'w+',
+                                                                                               newline='') as test:
+        reader = csv.reader(combinedcsv)
+        train_writer = csv.writer(train)
+        test_writer = csv.writer(test)
+
+        header = next(reader)
+        train_writer.writerow(header)
+        test_writer.writerow(header)
+
+        for row in reader:
+            sample_type = row[2]
+            if sample_type == "keyword":
+                train_writer.writerow(row)
+            elif sample_type == "random":
+                test_writer.writerow(row)
+
+def main():
+    bragging_data = '../data/bragging_data.csv'
+    train = '../data/keyword.csv'
+    test = '../data/random.csv'
+
+    split_data(bragging_data, train, test)
+
+main()
