eca7b6fd68075f299842f2f52f5f1f7b6b6a7a2e
chtreutel <claretreutel@proton.me>
2024-03-18 00:42:20 -0400

    finished preprocessing and added training step

--- src/main.py
+++ src/main.py
@@ -1,24 +1,32 @@
-# used some template text from the tutorial on these blogs:
+# NLP project
+# Detecting Bragging - Jin et al.
+# Clare Treutel, Duncan Collins, Ryan Connolly
+# Created: 3/15/24
+# Updated: 3/17/24
+
+# used some template text from the tutorials on these blogs:
+# https://huggingface.co/learn/nlp-course/en/chapter3/3
 # https://www.tensorflow.org/text/tutorials/classify_text_with_bert
 # https://www.kaggle.com/code/harshjain123/bert-for-everyone-tutorial-implementation
 # https://colab.research.google.com/github/NielsRogge/Transformers-Tutorials/blob/master/BERT/Fine_tuning_BERT_(and_friends)_for_multi_label_text_classification.ipynb
 
-import os
-import shutil
-import csv
 import numpy as np
-import pandas as pd
-import torch
-import torch.nn as nn
-from datasets import load_dataset
-from transformers import AutoTokenizer
-
+import csv
+import evaluate
 import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_text as text
-from official.nlp import optimization  # to create AdamW optimizer
+from datasets import load_dataset
+from datasets import ClassLabel
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from transformers import TrainingArguments, Trainer
 
-import matplotlib.pyplot as plt
+# import os
+# import shutil
+# import pandas as pd
+# import torch
+# import tensorflow_hub as hub
+# import tensorflow_text as text
+# import matplotlib.pyplot as plt
 
 tf.get_logger().setLevel('ERROR')
 
@@ -29,7 +37,7 @@ def split_data(combined, keyword, random):
         reader = csv.reader(combinedcsv)
         train_writer = csv.writer(train)
         test_writer = csv.writer(test)
-        cols = [1,4]
+        cols = [1, 4]
 
         header = next(reader)
         train_writer.writerow(header[n] for n in cols)
@@ -42,30 +50,53 @@ def split_data(combined, keyword, random):
             elif sample_type == "random":
                 test_writer.writerow(row[n] for n in cols)
 
-# def preprocess(data):
-#   TODO: preprocess/tokenize dataset
+
+def preprocess(data):
+    labels = ClassLabel(names_file='../data/labels.txt')
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    tok = tokenizer(data['text'], padding='max_length')
+    tok["label"] = labels.str2int(data['label'])
+    return tok
+
+
+def compute_metrics(eval_pred):
+    accuracy = evaluate.load("accuracy")
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
 
 def main():
     bragging_data = '../data/bragging_data.csv'
     train = '../data/train.csv'
     test = '../data/test.csv'
 
-#    uncomment this to split data from original bragging_data.csv
-#    split_data(bragging_data, train, test)
+    #    uncomment this to split data from original bragging_data.csv
+    #    split_data(bragging_data, train, test)
 
-    dataset = load_dataset("csv",data_files={"train":[train],"test":[test]})
-    labels = [label for label in dataset['train'].features.keys() if label not in ['text']]
-    id2label = {idx:label for idx, label in enumerate(labels)}
-    label2id = {label:idx for idx, label in enumerate(labels)}
-
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    dataset = load_dataset("csv", data_files={"train": [train], "test": [test]})
     labels = [label for label in dataset['train'].features.keys() if label not in ['text']]
     id2label = {idx: label for idx, label in enumerate(labels)}
     label2id = {label: idx for idx, label in enumerate(labels)}
 
-    # TODO: call preprocess fn
-    # TODO: define model
-    # TODO: train model
+    model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=7).to("cuda")
+    enc_data = dataset.map(preprocess, batched=True)
+    print(enc_data)
+
+    training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=enc_data["train"],
+        eval_dataset=enc_data["test"],
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
+
     # TODO: evaluate
+    # predictions = trainer.predict(enc_data["test"])
+    # preds = compute_metrics(predictions)
+    # metric = evaluate.load()
+
 
 main()
